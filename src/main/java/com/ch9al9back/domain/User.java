1de30dfc3fca7e5efbe07289aa39d98d5486b159
kimhyerin0909 <khyerin0909@gmail.com>
2022-09-08 19:04:27 +0900

    Feat - user entity

--- src/main/java/com/ch9al9back/domain/User.java
+++ src/main/java/com/ch9al9back/domain/User.java
@@ -1,4 +1,23 @@
 package com.ch9al9back.domain;
 
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
 public class User {
+    private int user_id;
+    private String id;
+    private String pw;
+    private String nickname;
+    private String hour;
+    private int salary;
+    public User(int user_id, String id, String pw, String nickname, String hour, int salary) {
+        this.user_id = user_id;
+        this.id = id;
+        this.pw = pw;
+        this.nickname = nickname;
+        this.hour = hour;
+        this.salary = salary;
+    }
 }
