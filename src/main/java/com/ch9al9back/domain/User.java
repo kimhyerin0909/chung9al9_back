0c2b0868fa5dd5fa8461550b99ee4ab5db5666f8
kimhyerin0909 <khyerin0909@gmail.com>
2022-09-15 17:22:38 +0900

    Fix - user dto 수정

--- src/main/java/com/ch9al9back/domain/User.java
+++ src/main/java/com/ch9al9back/domain/User.java
@@ -1,23 +1,24 @@
 package com.ch9al9back.domain;
 
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 @Getter
+@Setter
 @ToString
 public class User {
     private int user_id;
     private String id;
     private String pw;
     private String nickname;
-    private String hour;
-    private int salary;
-    public User(int user_id, String id, String pw, String nickname, String hour, int salary) {
+//    private String hour;
+//    private int salary;
+    public User(int user_id, String id, String pw, String nickname) {
         this.user_id = user_id;
         this.id = id;
         this.pw = pw;
         this.nickname = nickname;
-        this.hour = hour;
-        this.salary = salary;
     }
+
 }
