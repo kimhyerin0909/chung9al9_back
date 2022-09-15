ec0cea67fc275c6a95ca458a99d538d715294fdd
kimhyerin0909 <khyerin0909@gmail.com>
2022-09-15 17:21:51 +0900

    Feat - 비밀번호 암호화

--- src/main/java/com/ch9al9back/service/AuthService.java
+++ src/main/java/com/ch9al9back/service/AuthService.java
@@ -1,4 +1,18 @@
 package com.ch9al9back.service;
 
+import com.ch9al9back.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AuthService {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public void encodePW(User user) {
+        String enPw = passwordEncoder.encode(user.getPw());
+        System.out.println(enPw);
+        user.setPw(enPw);
+    }
 }
