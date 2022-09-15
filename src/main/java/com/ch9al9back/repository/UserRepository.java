bc326fdfce9a850648354fbe08ba94d557ed13a9
kimhyerin0909 <khyerin0909@gmail.com>
2022-09-15 17:23:07 +0900

    Feat - 유저인지 확인

--- src/main/java/com/ch9al9back/repository/UserRepository.java
+++ src/main/java/com/ch9al9back/repository/UserRepository.java
@@ -3,11 +3,18 @@ package com.ch9al9back.repository;
 import com.ch9al9back.domain.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
+
+import java.util.Map;
 
 @Repository
 @Mapper
 public interface UserRepository {
-    @Insert("insert into User values (#{user_id}, #{id}, #{pw}, #{nickname}, #{hour}, #{salary})")
+    @Insert("insert into User values (0, #{id}, #{pw}, #{nickname}, '0', 0)")
     void addUser(User user);
+
+    @Select("select * from User where id = #{id}")
+    Map<String, Object> findById(String id);
+
 }
