fddfaace484591cf36e5c2064500bf01c410c281
kimhyerin0909 <khyerin0909@gmail.com>
2022-09-15 17:20:20 +0900

    Feat - 비밀번호 암호화, jwt 발급

--- src/main/java/com/ch9al9back/controller/AuthController.java
+++ src/main/java/com/ch9al9back/controller/AuthController.java
@@ -2,25 +2,49 @@ package com.ch9al9back.controller;
 
 import com.ch9al9back.domain.User;
 import com.ch9al9back.repository.UserRepository;
+import com.ch9al9back.service.AuthService;
+import com.ch9al9back.service.JwtService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 public class AuthController {
+    @Autowired
     private final UserRepository userRepository;
+
+    @Autowired
+    private JwtService jwtService;
+    @Autowired
+    private AuthService authService;
 
     public AuthController(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
     @RequestMapping(value = "/join", method = RequestMethod.POST)
-    public void Join(@RequestBody User user) throws Exception {
+    public String Join(@RequestBody User user) throws Exception {
         // 회원가입
-        System.out.println(user);
-        userRepository.addUser(user);
+        Map<String, Object> isUser = userRepository.findById(user.getId());
+        if(isUser != null) return "이미 가입된 회원입니다.";
+        else {
+            authService.encodePW(user);
+            userRepository.addUser(user);
+            return jwtService.createJWT(user);
+        }
     }
 
     @RequestMapping("/login")
-    public void Login(@ModelAttribute User user) throws Exception {
+    public String Login(@RequestBody User user) throws Exception {
         // 로그인
+        Map<String, Object> isUser = userRepository.findById(user.getId());
+        if(isUser == null) return "회원가입 필요";
+        else return jwtService.createJWT(user);
+    }
+
+    @RequestMapping("/pwtest")
+    public void PwTest(@RequestBody User user) throws Exception {
+        authService.encodePW(user);
     }
 }
