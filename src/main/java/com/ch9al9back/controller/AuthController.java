d55ff6604b6fe4bbf93944cb88fd392a38c50039
kimhyerin0909 <khyerin0909@gmail.com>
2022-12-12 00:51:05 +0900

    Feat - return JSON, get user info by user id
    로그인 예외처리 JSON으로 return, user id로 정보 가져오기

--- src/main/java/com/ch9al9back/controller/AuthController.java
+++ src/main/java/com/ch9al9back/controller/AuthController.java
@@ -4,14 +4,18 @@ import com.ch9al9back.domain.User;
 import com.ch9al9back.repository.UserRepository;
 import com.ch9al9back.service.AuthService;
 import com.ch9al9back.service.JwtService;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.Resource;
 import java.util.Map;
 
 @RestController
 public class AuthController {
-    @Autowired
+
+    @Resource(name = "userRepository")
     private final UserRepository userRepository;
     @Autowired
     private JwtService jwtService;
@@ -34,11 +38,28 @@ public class AuthController {
     }
 
     @RequestMapping("/login") // 로그인
-    public String Login(@RequestBody User user) throws Exception {
+    public JSONObject Login(@RequestBody User user) throws Exception {
         Map<String, Object> isUser = userRepository.findById(user.getId());
         String pw = userRepository.pwCheck(user.getId());
-        if(isUser == null) return "회원가입 필요";
-        else if(!authService.encodePWForCheck(pw, user)) return "비밀번호 틀림";
-        else return jwtService.createJWT(user);
+        JSONObject result = new JSONObject();
+        if(isUser == null) {
+            result.put("success", false);
+            result.put("message", "회원가입이 필요합니다.");
+        }
+        else if(!authService.encodePWForCheck(pw, user)) {
+            result.put("success", false);
+            result.put("message", "비밀번호가 틀렸습니다.");
+        }
+        else {
+            result.put("success", true);
+            result.put("token", jwtService.createJWT(user));
+            result.put("info", isUser);
+        };
+        return result;
+    }
+
+    @RequestMapping(value = "/getUserInfo", method = RequestMethod.GET)
+    public Map<String, Object> GetUserInfo(@RequestParam long user_id) throws Exception {
+        return userRepository.findByUserId(user_id);
     }
 }
